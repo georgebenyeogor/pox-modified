f8bbbdfa17bdeb49f789afc219405d90827807e1
George <u1267058@utah.edu>
2025-03-09 17:47:55 -0600

    mock update

--- ext/George_Benyeogor_U1267058.py
+++ ext/George_Benyeogor_U1267058.py
@@ -1,109 +1,135 @@
-# Copyright 2013 <Your Name Here>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.packet.arp import arp
+from pox.lib.packet.ethernet import ethernet
+from pox.lib.addresses import IPAddr, EthAddr
+import pox.lib.packet as pkt
 
-"""
-A skeleton POX component
-
-You can customize this to do whatever you like.  Don't forget to
-adjust the Copyright above, and to delete the Apache license if you
-don't want to release under Apache (but consider doing so!).
-
-Rename this file to whatever you like, .e.g., mycomponent.py.  You can
-then invoke it with "./pox.py mycomponent" if you leave it in the
-ext/ directory.
-
-Implement a launch() function (as shown below) which accepts commandline
-arguments and starts off your component (e.g., by listening to events).
-
-Edit this docstring and your launch function's docstring.  These will
-show up when used with the help component ("./pox.py help --mycomponent").
-"""
-
-# Import some POX stuff
-from pox.core import core                     # Main POX object
-import pox.openflow.libopenflow_01 as of      # OpenFlow 1.0 library
-import pox.lib.packet as pkt                  # Packet parsing/construction
-from pox.lib.addresses import EthAddr, IPAddr # Address types
-import pox.lib.util as poxutil                # Various util functions
-import pox.lib.revent as revent               # Event library
-import pox.lib.recoco as recoco               # Multitasking library
-
-# Create a logger for this component
 log = core.getLogger()
 
 
-def _go_up (event):
-  # Event handler called when POX goes into up state
-  # (we actually listen to the event in launch() below)
-  log.info("Skeleton application ready (to do nothing).")
+VIRTUAL_IP = IPAddr("10.0.0.10")        # The "virtual" IP clients will ping
+SERVER_IPS = [IPAddr("10.0.0.5"),
+              IPAddr("10.0.0.6")]      # Real server IPs
+SERVER_MACS = [EthAddr("00:00:00:00:00:05"),
+               EthAddr("00:00:00:00:00:06")]  # Corresponding MACs
 
+class LoadBalancer (object):
+    """
+    Implements a simple round-robin load balancer with ARP interception.
+    """
+    def __init__(self):
+        # Keep track of which server is next (for round robin)
+        self.server_index = 0
 
-@poxutil.eval_args
-def launch (foo, bar = False):
-  """
-  The default launcher just logs its arguments
-  """
-  # When your component is specified on the commandline, POX automatically
-  # calls this function.
+        # Listen for OpenFlow events
+        core.openflow.addListeners(self)
+        log.info("LoadBalancer initialized")
 
-  # Add whatever parameters you want to this.  They will become
-  # commandline arguments.  You can specify default values or not.
-  # In this example, foo is required and bar is not.  You may also
-  # specify a keyword arguments catch-all (e.g., **kwargs).
+    def _handle_ConnectionUp(self, event):
+        """
+        Called when a switch connects to the controller.
+        """
+        log.info("Switch %s has connected", event.connection.dpid)
 
-  # For example, you can execute this component as:
-  # ./pox.py skeleton --foo=3 --bar=4
+    def _handle_PacketIn(self, event):
+        """
+        Called when a packet arrives that the switch doesn't know how to handle.
+        """
+        packet = event.parsed
+        inport = event.port
+        dpid = event.connection.dpid
 
-  # Note that arguments passed from the commandline are ordinarily
-  # always strings, and it's up to you to validate and convert them.
-  # The one exception is if a user specifies the parameter name but no
-  # value (e.g., just "--foo").  In this case, it receives the actual
-  # Python value True.
-  # The @pox.util.eval_args decorator interprets them as if they are
-  # Python literals.  Even things like --foo=[1,2,3] behave as expected.
-  # Things that don't appear to be Python literals are left as strings.
+        # 1. Handle ARP packets
+        if packet.type == ethernet.ARP_TYPE:
+            self._handle_arp(event, packet)
+            return
 
-  # If you want to be able to invoke the component multiple times, add
-  # __INSTANCE__=None as the last parameter.  When multiply-invoked, it
-  # will be passed a tuple with the following:
-  # 1. The number of this instance (0...n-1)
-  # 2. The total number of instances for this module
-  # 3. True if this is the last instance, False otherwise
-  # The last is just a comparison between #1 and #2, but is convenient.
+        # 2. Handle IP packets (ICMP)
+        elif packet.type == ethernet.IP_TYPE:
+            self._handle_ip(event, packet)
+            return
 
-  log.warn("Foo: %s (%s)", foo, type(foo))
-  log.warn("Bar: %s (%s)", bar, type(bar))
+        log.debug("Ignoring non-ARP, non-IP packet")
 
-  core.addListenerByName("UpEvent", _go_up)
+    def _handle_arp(self, event, packet):
+        """
+        Intercept ARP requests for the virtual IP and respond with
+        the chosen server's MAC address. Also install flow rules.
+        """
+        arp_req = packet.find('arp')
+        if not arp_req:
+            return
 
+        # Check if ARP is a request for the VIRTUAL_IP
+        if arp_req.opcode == arp.REQUEST and arp_req.protodst == VIRTUAL_IP:
+            # 1. Select a server in round-robin fashion
+            server_ip = SERVER_IPS[self.server_index]
+            server_mac = SERVER_MACS[self.server_index]
 
-def breakfast ():
-  """
-  Serves a Pythonic breakfast
-  """
-  # You can invoke other functions from the commandline too.  We call
-  # these multiple or alternative launch functions.  To execute this
-  # one, you'd do:
-  # ./pox.py skeleton:breakfast
+            # 2. Bump the index for the next request
+            self.server_index = (self.server_index + 1) % len(SERVER_IPS)
 
-  import random
-  items = "egg,bacon,sausage,baked beans,tomato".split(',')
-  random.shuffle(items)
-  breakfast = items[:random.randint(0,len(items))]
-  breakfast += ['spam'] * random.randint(0,len(breakfast)+1)
-  random.shuffle(breakfast)
-  if len(breakfast) == 0: breakfast = ["lobster thermidor aux crevettes"]
+            # 3. Craft an ARP reply
+            arp_reply = arp()
+            arp_reply.opcode = arp.REPLY
+            arp_reply.hwsrc = server_mac       
+            arp_reply.hwdst = arp_req.hwsrc
+            arp_reply.protosrc = VIRTUAL_IP
+            arp_reply.protodst = arp_req.protosrc
 
-  log.warn("Breakfast is served:")
-  log.warn("%s and spam", ", ".join(breakfast))
+            ether = ethernet()
+            ether.type = ethernet.ARP_TYPE
+            ether.src = server_mac
+            ether.dst = packet.src
+            ether.set_payload(arp_reply)
+
+            # 4. Send ARP reply out the same port the request came in
+            msg = of.ofp_packet_out()
+            msg.data = ether.pack()
+            msg.actions.append(of.ofp_action_output(port = event.port))
+            event.connection.send(msg)
+
+            self._install_flow_rules(event.connection, event.port, server_ip, server_mac, arp_req.protosrc, arp_req.hwsrc)
+
+    def _handle_ip(self, event, packet):
+        """
+        Handle IP packets if they somehow arrive here without flows.
+        Typically, if you set up flows properly on ARP, this might not be used as much.
+        """
+        log.debug("Received IP packet %s", packet.find('ipv4'))
+
+    def _install_flow_rules(self, connection, inport, server_ip, server_mac, client_ip, client_mac):
+        """
+        Install two flow rules:
+        1) Client -> Server:  Match on client -> Virtual IP, rewrite to server IP, server MAC
+        2) Server -> Client:  Match on server IP -> client IP, rewrite source IP to Virtual IP
+        """
+        # Flow 1: Client to Server
+        fm1 = of.ofp_flow_mod()
+        fm1.match.in_port = inport
+        fm1.match.dl_type = 0x0800          # IP type
+        fm1.match.nw_dst = VIRTUAL_IP       # Dest is the virtual IP
+
+        fm1.actions.append(of.ofp_action_nw_addr.set_dst(server_ip))
+        fm1.actions.append(of.ofp_action_dl_addr.set_dst(server_mac))
+        
+        connection.send(fm1)
+
+        # Flow 2: Server to Client
+        fm2 = of.ofp_flow_mod()
+
+        fm2.match.dl_type = 0x0800
+        fm2.match.nw_src = server_ip
+        fm2.match.nw_dst = client_ip
+
+        fm2.actions.append(of.ofp_action_nw_addr.set_src(VIRTUAL_IP))
+
+        connection.send(fm2)
+
+def launch():
+    """
+    POX will automatically call this 'launch' function when you do:
+    python pox.py openflow.of_01 --port=6633 George_Benyeogor_U1267058
+    """
+    core.registerNew(LoadBalancer)
